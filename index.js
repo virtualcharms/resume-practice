969b81c3ae2a3875968510ec0cf816b962baa792
virtualcharms <sodalitemix@gmail.com>
2020-08-05 09:43:42 -0700

    Editing errors

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
 const fs = require('fs')
 
 fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
-    if(err) {
-        console.log(err) {
-            
-        } else {
-            console.log(data)
-            return data
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+        return data
     }
 })
